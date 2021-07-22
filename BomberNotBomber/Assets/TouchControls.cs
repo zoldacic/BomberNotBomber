a5d3abe6c1e830169d9626ba724d3efd88f5d225
Fredrik Lundgren <ext.fredrik.lundgren@tieto.com>
2021-07-22 02:20:22 +0200

    Added mlapi package

--- BomberNotBomber/Assets/TouchControls.cs
+++ BomberNotBomber/Assets/TouchControls.cs
@@ -19,61 +19,23 @@ public class @TouchControls : IInputActionCollection, IDisposable
             ""id"": ""56f9ab1c-2f80-420a-9889-4a34ce20f974"",
             ""actions"": [
                 {
-                    ""name"": ""TouchInput"",
-                    ""type"": ""PassThrough"",
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
                     ""id"": ""b761e90c-aeb3-4561-9d34-778a4608223c"",
-                    ""expectedControlType"": """",
-                    ""processors"": """",
-                    ""interactions"": """"
-                },
-                {
-                    ""name"": ""TouchPress"",
-                    ""type"": ""Button"",
-                    ""id"": ""8beef481-c9fd-42a2-9395-6809fe13a8c7"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": ""Press(behavior=2)""
-                },
-                {
-                    ""name"": ""TouchPosition"",
-                    ""type"": ""PassThrough"",
-                    ""id"": ""71f419e9-d4fe-438b-a6b0-d005b72f8a2d"",
                     ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
-                    ""interactions"": ""Press(behavior=2)""
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
                 {
                     ""name"": """",
                     ""id"": ""e865b7e9-e3ef-4885-aee8-c12d126c0cee"",
-                    ""path"": ""<Touchscreen>/primaryTouch"",
+                    ""path"": ""<Gamepad>/leftStick"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""TouchCS"",
-                    ""action"": ""TouchInput"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""d6ba717f-34ba-4c9d-a8f9-1c7014ea0d67"",
-                    ""path"": ""<Touchscreen>/primaryTouch/press"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""TouchCS"",
-                    ""action"": ""TouchPress"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""c9b8db54-0a9b-497b-9373-03d5b39e2ccd"",
-                    ""path"": ""<Touchscreen>/primaryTouch/position"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""TouchCS"",
-                    ""action"": ""TouchPosition"",
+                    ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -107,9 +69,7 @@ public class @TouchControls : IInputActionCollection, IDisposable
 }");
         // Gameplay
         m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
-        m_Gameplay_TouchInput = m_Gameplay.FindAction("TouchInput", throwIfNotFound: true);
-        m_Gameplay_TouchPress = m_Gameplay.FindAction("TouchPress", throwIfNotFound: true);
-        m_Gameplay_TouchPosition = m_Gameplay.FindAction("TouchPosition", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -159,16 +119,12 @@ public class @TouchControls : IInputActionCollection, IDisposable
     // Gameplay
     private readonly InputActionMap m_Gameplay;
     private IGameplayActions m_GameplayActionsCallbackInterface;
-    private readonly InputAction m_Gameplay_TouchInput;
-    private readonly InputAction m_Gameplay_TouchPress;
-    private readonly InputAction m_Gameplay_TouchPosition;
+    private readonly InputAction m_Gameplay_Move;
     public struct GameplayActions
     {
         private @TouchControls m_Wrapper;
         public GameplayActions(@TouchControls wrapper) { m_Wrapper = wrapper; }
-        public InputAction @TouchInput => m_Wrapper.m_Gameplay_TouchInput;
-        public InputAction @TouchPress => m_Wrapper.m_Gameplay_TouchPress;
-        public InputAction @TouchPosition => m_Wrapper.m_Gameplay_TouchPosition;
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
         public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -178,28 +134,16 @@ public class @TouchControls : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
             {
-                @TouchInput.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchInput;
-                @TouchInput.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchInput;
-                @TouchInput.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchInput;
-                @TouchPress.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchPress;
-                @TouchPress.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchPress;
-                @TouchPress.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchPress;
-                @TouchPosition.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchPosition;
-                @TouchPosition.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchPosition;
-                @TouchPosition.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnTouchPosition;
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
             }
             m_Wrapper.m_GameplayActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @TouchInput.started += instance.OnTouchInput;
-                @TouchInput.performed += instance.OnTouchInput;
-                @TouchInput.canceled += instance.OnTouchInput;
-                @TouchPress.started += instance.OnTouchPress;
-                @TouchPress.performed += instance.OnTouchPress;
-                @TouchPress.canceled += instance.OnTouchPress;
-                @TouchPosition.started += instance.OnTouchPosition;
-                @TouchPosition.performed += instance.OnTouchPosition;
-                @TouchPosition.canceled += instance.OnTouchPosition;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
             }
         }
     }
@@ -224,8 +168,6 @@ public class @TouchControls : IInputActionCollection, IDisposable
     }
     public interface IGameplayActions
     {
-        void OnTouchInput(InputAction.CallbackContext context);
-        void OnTouchPress(InputAction.CallbackContext context);
-        void OnTouchPosition(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Asset locations
 
 https://pixelfrog-assets.itch.io/pirate-bomb
+
+Input system tutorials:  https://www.youtube.com/watch?v=ERAN5KBy2Gs, https://www.youtube.com/watch?v=zd75Jq37R60
